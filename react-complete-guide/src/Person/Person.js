40e85a0b6248fc24053cc7c73a33c7b3de7cf27c
Jamie <jamie03.v@gmail.com>
2020-03-01 17:03:43 -0500

    inline styling and separate css styling

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,12 @@
 //Creating components- in its simplest form is just a function that returns some JSX(html).
 
-//we need to import React
+//we need to import React and our styling CSS
 import React from 'react';
+import './Person.css'
 
 const person = props => {
 return (
-    <div>
+    <div className="Person">
         <p onClick={props.clicky}>I'm a {props.name} Person and I am {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.inputChanged} value={props.name} />
