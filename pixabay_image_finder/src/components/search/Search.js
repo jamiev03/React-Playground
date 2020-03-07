e906e11cdb7074fda2a27748c9f71787ba708cd7
Jamie <jamie03.v@gmail.com>
2020-03-07 15:00:04 -0500

    working through a CORS error

--- pixabay_image_finder/src/components/search/Search.js
+++ pixabay_image_finder/src/components/search/Search.js
@@ -13,22 +13,43 @@ class Search extends Component {
         images: [] //is an empty array to begin with and once we make a request and get images to fill this array
     }
 
-    onTextChange = (e) => { //as a parameter it takes the event.
-        //set the searchText in state to the value in TextField.
-        //pass in a callback when the name changes to value-
-        this.setState({[e.target.name]: e.target.value}, () => {
-            //using axios to make a 'get' request
-            axios.get(`${this.state.apiUrl}/?key=
-            ${this.state.apiKey}&q=
-            ${this.state.searchText}
-            &image_type=photo&per_page
-            ${this.state.amount}&safesearch=true`)//axios get request gives promise and now we need to access the 'hits' array from the JSON and insert it into the 'image' array- which is empty.
-            .then(res => this.setState({images: res.data.hits}))
-            .catch(err => console.log(err));//.catch logs error
-        });
-    }
+    // onTextChange = (e) => { //as a parameter it takes the event.
+    //     //set the searchText in state to the value in TextField.
+    //     //pass in a callback when the name changes to value-
+    //     this.setState({[e.target.name]: e.target.value}, () => {
+    //         //using axios to make a 'get' request
+    //         axios.get(`${this.state.apiUrl}/?key=
+    //         ${this.state.apiKey}&q=
+    //         ${this.state.searchText}
+    //         &image_type=photo&per_page
+    //         ${this.state.amount}&safesearch=true`,
+    //         )//axios get request gives promise and now we need to access the 'hits' array from the JSON and insert it into the 'image' array- which is empty.
+    //         .then(res => this.setState({images: res.data.hits}))
+    //         .catch(err => console.log(err));//.catch logs error
+    //     });
+    // }
+
+    
+  onTextChange = e => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
+    });
+  };
 
     render() {
+        console.log(this.state.images);
         return (
             <div>
                 <TextField //Using TextField component from MUI
