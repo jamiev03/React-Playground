062ae234779b8c1faec8891aac31dd05ed00c03a
Jamie <jamie03.v@gmail.com>
2020-03-09 20:16:59 -0400

    New todo app

---
+++ todo-app/src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Todos extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default Todos;
