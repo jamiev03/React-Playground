6b0592913de787ed09d6a05497c93a75e27b8924
Jamie <jamie03.v@gmail.com>
2020-03-13 19:40:13 -0400

    About.js, create about page, Header updated using Router to add links to About

--- todo-app/src/components/Header.js
+++ todo-app/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; //import 'Link' from react router
 
 function Header() {
     return (
         <header style={headerStyles}>
             <h1>Todo List</h1>
+            <Link style={linkStyles} to="/">Home</Link> | <Link style={linkStyles} to="/about">About</Link>
         </header>
     )
 }
@@ -14,6 +16,11 @@ const headerStyles = {
     textAlign: 'center',
     fontWeight: '900',
     padding: '1rem'
+}
+
+const linkStyles = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header
