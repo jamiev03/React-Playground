d5bb475f505969d766e3742c674cfb12ce96f3e6
Jamie <jamie03.v@gmail.com>
2020-03-21 19:10:18 -0400

    styling of components using the css modules

--- dev-react-beginners/src/components/Recipe.js
+++ dev-react-beginners/src/components/Recipe.js
@@ -4,14 +4,15 @@ import style from '../modules/recipe.module.css'
 const Recipe = ( {title, calories, image, ingredients } ) => {
     return (
         <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
+            <h1 className={style.title}>{title}</h1>
+            <ol className={style.list}>
             {/* map over the ingredients object and for each iteration we return ingredient.text within an li. Note when returning html the arrow function req () type brackets. */}
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>{calories}</p>
+            <p>Calories:</p>
+            <p className={style.calories}>{Math.floor(calories)}</p>
             <img className={style.image} src={image} alt=""/>
         </div>
     )
