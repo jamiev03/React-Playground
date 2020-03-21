58db4c78be1177b57845d7b1d8194b6e91a8ef7b
Jamie <jamie03.v@gmail.com>
2020-03-20 21:48:00 -0400

    get API keys and set to const

--- dev-react-beginners/src/App.js
+++ dev-react-beginners/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
 const App = () => {
@@ -6,11 +6,23 @@ const App = () => {
   const APP_ID = '0OdUWJ0sBjDrqHygGUXeCF';
   const APP_KEY = 'BQAIERWZe0pP14FXz58Yyv5UGSQ08MmrR-hB9Z0DI9Ubd24sTyryqMgFqhlhexGkZpymUJW3jsf7hV3p3HGUG3PT2gkdYf9Mj3ORiE6B596mPGEHgquM18fPkwQ7QmOMbc2CadeAHjydmibE2Bam7rx417iwaTg';
 
-  const exampleReq = `https://api.spotify.com/v1/artists/{${APP_ID}}`;
+  const exampleReq = `https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF" -H "Authorization: Bearer {${APP_KEY}}`;
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  })
 
 
   return (
-    <div className="App">Jo tere</div>
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button onClick={() => setCounter(counter + 1)}className="search-button" 
+        type="submit">{counter}</button>
+      </form>
+    </div>
   );
 }
 
