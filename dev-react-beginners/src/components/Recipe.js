bcafc5c21bd391f2e6a06040ead867dcdcca1eac
Jamie <jamie03.v@gmail.com>
2020-03-22 20:13:24 -0400

    fix key prop error by adding an id iterator as the key. Setting it to 0 up top.

--- dev-react-beginners/src/components/Recipe.js
+++ dev-react-beginners/src/components/Recipe.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from '../modules/recipe.module.css'
 
-const Recipe = ( {title, calories, image, ingredients } ) => {
+const Recipe = ( {title, calories, image, ingredients, source } ) => {
+    let id = 0;
     return (
         <div className={style.recipe}>
             <h1 className={style.title}>{title}</h1>
             <ul className={style.list}>
             {/* map over the ingredients object and for each iteration we return ingredient.text within an li. Note when returning html the arrow function req () type brackets. */}
                 {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
+                    <li key={id++}>{ingredient.text}</li>
                 ))}
             </ul>
             <p>Calories:</p>
