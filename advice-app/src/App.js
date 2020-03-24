794ac7b82aff7339365fb9de57cc7c8afa29b78d
Jamie <jamie03.v@gmail.com>
2020-03-23 21:05:04 -0400

    retrieve data from src and set state to advice- learned more about destructuring.

--- advice-app/src/App.js
+++ advice-app/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import Modal from './components/Modal';
+import axios from 'axios';
 
 class App extends Component {
+  //create initial state- empty string
   state = { advice: '' };
 
   componentDidMount() {
-    console.log('Component did mount');
+    this.getAdvice();
+  }
 
+  //getAdvice method. get request using axios
+  getAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+      const { advice } = response.data.slip;
+      console.log(advice);
+
+      //set the advice value retrieved from response.data.slip.advice to the advice in the state:
+      this.setState({ advice: advice })
+
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
 
   render() {
+    const { advice } = this.state;
+
     return (
+      <div>
+        <h1>{advice}</h1>
+        <Modal />
+      </div>
       
-      <Modal />
     )
   }
 }
