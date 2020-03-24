a5a2e6292a9bbf15091af8fa409ac6c062ad38c5
Jamie <jamie03.v@gmail.com>
2020-03-23 20:35:41 -0400

    starting new advice app

---
+++ advice-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi There!</h1>
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
---
+++ advice-app/src/components/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modal = ( props ) => {
+    return (
+        <div className='modal'>
+        <h1>this is a modal</h1>
+        </div>
+    )
+}
+
+export default Modal;
