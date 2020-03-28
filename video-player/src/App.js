8f1ddc58e458dd22fa65bfd51f69d707282069d5
Jamie <jamie03.v@gmail.com>
2020-03-27 21:00:41 -0400

    import material-ui and axios, set up the api call

--- video-player/src/App.js
+++ video-player/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hi there!</h1>
-            </div>
+            <Grid justify='center' container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+
+                        </Grid>
+                        <Grid item xs={8}>
+                            
+                        </Grid>
+                        <Grid item xs={4}>
+                            
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
