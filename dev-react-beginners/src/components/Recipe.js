532e812be03374d319c5febd8a710f768cd5bdd7
Jamie <jamie03.v@gmail.com>
2020-03-28 20:43:51 -0400

    created new starts component to add a rating to each item

--- dev-react-beginners/src/components/Recipe.js
+++ dev-react-beginners/src/components/Recipe.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from '../modules/recipe.module.css'
+import Stars from './Stars';
 
 const Recipe = ( {title, calories, image, ingredients } ) => {
     let id = 0;
     return (
         <div className={style.recipe}>
             <h1 className={style.title}>{title}</h1>
+            <Stars />
             <ul className={style.list}>
             {/* map over the ingredients object and for each iteration we return ingredient.text within an li. Note when returning html the arrow function req () type brackets. */}
                 {ingredients.map(ingredient => (
---
+++ dev-react-beginners/src/components/Stars.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Stars = () => {
+    return (
+        <div>
+         <span class="fa fa-star checked"></span>
+         <span class="fa fa-star checked"></span>
+         <span class="fa fa-star checked"></span>
+         <span class="fa fa-star"></span>
+         <span class="fa fa-star"></span>   
+        </div>
+    )
+}
+
+export default Stars;
