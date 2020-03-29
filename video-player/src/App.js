8a2bccf5c8d310ed1ef81ade85acf14cc1ebb1ad
Jamie <jamie03.v@gmail.com>
2020-03-29 11:50:09 -0400

    videoList component created

--- video-player/src/App.js
+++ video-player/src/App.js
@@ -4,7 +4,7 @@ import youtube from './api/youtube';
 //have created a separate import file index.js to hold the exports:
 // export { default as SearchBar } from './SearchBar';
 // export { default as VideoDetails } from './VideoDetails';
-import { SearchBar, VideoDetails } from './components';
+import { SearchBar, VideoDetails, VideoList } from './components';
 
 class App extends Component {
     state = {
---
+++ video-player/src/components/VideoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Grid, Paper, Typography } from '@material-ui/core';
+
+const VideoItem = () => {
+    return (
+        <h1>video Item</h1>
+    )
+}
+
+export default VideoItem;
