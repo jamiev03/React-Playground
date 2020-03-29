d2d71674dd521c95a430d76dfbc6612b91245f2d
Jamie <jamie03.v@gmail.com>
2020-03-29 12:16:30 -0400

    VideoItem styling, new module and data input

--- video-player/src/components/VideoItem.js
+++ video-player/src/components/VideoItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Grid, Paper, Typography } from '@material-ui/core';
+import VideoItemStyle from '../modules/VideoItemStyle.module.css';
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
     return (
-        <h1>video Item</h1>
+        <Grid item xs={12}>
+            <Paper className={VideoItemStyle.paper}>
+                <img className={VideoItemStyle.img} alt="thumbnail" src={video.snippet.thumbnails.medium.url} />
+                <Typography className={VideoItemStyle.subtitle}variant="subtitle1">{video.snippet.title}</Typography>
+            </Paper>
+        </Grid>
     )
 }
 
