3b6d89371fb6d6b0941d0bd5ec8fcee7db57befc
Jamie <jamie03.v@gmail.com>
2020-04-02 20:52:48 -0400

    Learning specifics of lifecylce methods

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js], constructor');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -13,6 +18,11 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js], getDerivedStateFromProps', props);
+    return state;
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -46,8 +56,8 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js], render');
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = <Persons 
