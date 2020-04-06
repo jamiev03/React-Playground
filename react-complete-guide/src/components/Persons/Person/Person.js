e505f3e3e7aa56615c3a940ca177345002ca593b
Jamie <jamie03.v@gmail.com>
2020-04-05 20:12:25 -0400

    using referece hooks in both class and functional components

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -5,6 +5,15 @@ import classes from './Person.css';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js], rendering...');
     return (
@@ -15,7 +24,12 @@ class Person extends Component {
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input 
+        type="text" 
+        onChange={this.props.changed} 
+        value={this.props.name} 
+        ref={this.inputElementRef} 
+        />
       
       </Auxillary>
     );
