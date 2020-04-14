53b14f2128764a07ac89d5b41188b75296c6556b
Jamie <jamie03.v@gmail.com>
2020-04-14 19:51:22 -0400

    NAvigation component created. Applied basic styling for nav items and nav bar

---
+++ hamburger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from '../../../modules/NavigationItems.module.css';
+
+const navigationItems = (props) => (
+    <ul>
+        <li className={classes.NavigationItems}><a href="/">A Link</a></li>
+    </ul>;
+)
+
+export default navigationItems;
