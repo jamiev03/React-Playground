034055f8911aa49728fc5f23b7a7c972ff0d9c26
Jamie <jamie03.v@gmail.com>
2020-04-14 20:31:13 -0400

    Working on styling for responsive sideDrawer component.

--- hamburger-builder/src/components/Layout/Layout.js
+++ hamburger-builder/src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import LayoutStyle from '../../modules/Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Auxillary>
         <Toolbar />
+        <SideDrawer />
         <main className={LayoutStyle.content}>
             {props.children}
         </main>
--- hamburger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ hamburger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from '../../../modules/SideDrawer.module.css';
 
 const sideDrawer = (props) => {
     return (
-
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}><Logo /></div>
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
     );
 };
 
