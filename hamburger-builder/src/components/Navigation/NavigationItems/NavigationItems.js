011e099a77f5e34f2bb497791a23569102812095
Jamie <jamie03.v@gmail.com>
2020-04-14 20:04:38 -0400

    side drawer component, set styling for navigationItems

--- hamburger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ hamburger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,9 +2,14 @@ import React from 'react';
 import classes from '../../../modules/NavigationItems.module.css';
 
 const navigationItems = (props) => (
-    <ul>
-        <li className={classes.NavigationItems}><a href="/">A Link</a></li>
-    </ul>;
-)
+    <ul className={classes.NavigationItems}>
+        <li className={classes.NavigationItem}><a
+         href={props.link}
+         className={props.active ? classes.active : null}>Burger Builder</a></li>
+         <li className={classes.NavigationItem}><a
+         href={props.link}
+         className={props.active ? classes.active : null}>Checkout</a></li>
+    </ul>
+);
 
 export default navigationItems;
