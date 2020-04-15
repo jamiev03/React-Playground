433cf7faf18287adfd0090353bb1952e8f6825ed
Jamie <jamie03.v@gmail.com>
2020-04-15 19:25:51 -0400

    adding backdrop to sideDrawer component, switch Layout.js to a class component in order to monitor state of Toolbar and SideDrawer

--- hamburger-builder/src/components/Layout/Layout.js
+++ hamburger-builder/src/components/Layout/Layout.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import LayoutStyle from '../../modules/Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Auxillary>
-        <Toolbar />
-        <SideDrawer />
-        <main className={LayoutStyle.content}>
-            {props.children}
-        </main>
-    </Auxillary>
-)
+class Layout extends Component {
+    render() {
+        return (
+            <Auxillary>
+                <Toolbar />
+                <SideDrawer />
+                <main className={LayoutStyle.content}>
+                    {this.props.children}
+                </main>
+            </Auxillary>
+        )
+    } 
+}
 
-export default layout;
+export default Layout;
--- hamburger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ hamburger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from '../../../modules/SideDrawer.module.css';
+import Backdrop from '../../UI/Modal/Backdrop/Backdrop';
+import Auxillary from '../../../hoc/Auxillary';
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}><Logo /></div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Auxillary>
+            <Backdrop show />
+                <div className={classes.SideDrawer}>
+                    <div className={classes.Logo}><Logo /></div>
+                    <nav>
+                        <NavigationItems />
+                    </nav>
+                </div>
+        </Auxillary>
     );
 };
 
