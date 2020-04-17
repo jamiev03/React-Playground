dcc337bf3a26fef450fbf8b6297a55d4a6d19249
Jamie <jamie03.v@gmail.com>
2020-04-16 20:18:16 -0400

    looking for improvements to app performance- re- rendering of order summary when modal is not visible.

--- hamburger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ hamburger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxillary from '../../../hoc/Auxillary';
 import Button from '../../UI/Modal/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>
         )
         })
@@ -18,12 +19,13 @@ const orderSummary = (props) => {
            <ul>
                {ingredientSummary}
            </ul>
-           <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+           <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
            <p>Proceed to Checkout?</p>
-           <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-           <Button btnType='Success' clicked={props.purchaseContinued}>Order Now</Button>
+           <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+           <Button btnType='Success' clicked={this.props.purchaseContinued}>Order Now</Button>
        </Auxillary> 
     )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
