c32bdacb93ba589ef747dd395cf8f738467f88cd
Jamie <jamie03.v@gmail.com>
2020-04-27 21:04:34 -0400

    learning about interceptors which run globally when any request is made from or to your app

--- reaching-out-to-the-web/src/index.js
+++ reaching-out-to-the-web/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Axios from 'axios';
+
+Axios.interceptors.request.use(request => {
+    console.log(request);
+    //Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+});
+
+Axios.interceptors.response.use(response => {
+    console.log(response);
+    //Edit response config
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
